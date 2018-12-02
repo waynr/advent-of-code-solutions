d4427de2571117e51d0ddb882f25a306e8eaa61c
Wayne Warren <wayne.warren.s@gmail.com>
2018-12-02 15:52:56 -0600

    Solve day1 part2, generalize input acquisition

--- aoc2018/src/main.rs
+++ aoc2018/src/main.rs
@@ -1,21 +1,73 @@
 extern crate reqwest;
 
-fn main() -> Result<(), Box<std::error::Error>> {
+use std::collections::HashSet;
+
+struct AdventDay {
+    year: u16,
+    day: u8,
+}
+
+fn get_input(day: AdventDay, session_id: String) ->  Result<String, Box<std::error::Error>> {
     let cli = reqwest::Client::new();
-    let req = cli
-        .get("https://adventofcode.com/2018/day/1/input")
-        .header(reqwest::header::COOKIE, "session=53616c7465645f5f1925b812ca83a437cb646c356e8f9061af30c5a7da04361914e658d95444af03c01c482303375958");
+    let url = format!("https://adventofcode.com/{}/day/{}/input", day.year, day.day);
+    let cookie = format!("session={}", session_id);
+
+    //println!("{}", url);
+    //println!("{}", session_id);
+    let req = cli.get(&url)
+        .header(reqwest::header::COOKIE, cookie);
+    //println!("{:?}", req);
 
     let mut res = req.send()?;
 
-    let freq: i32  = res
-        .text()?
+    //println!("{:?}", res);
+    //println!("{}", res.text()?);
+    Ok(res.text()?.to_string())
+}
+
+fn main() -> Result<(), Box<std::error::Error>> {
+    let advent_day: AdventDay = AdventDay {
+        year: 2018,
+        day: 1,
+    };
+
+    let session_id = String::from("meow");
+    let input = get_input(advent_day, session_id)?;
+    //println!("{}", input);
+
+    let freq_deltas: Vec<i32> = input
         .split('\n')
         .map(|s: &str| s.to_string().parse::<i32>().ok())
         .filter_map(|e| e)
+        .collect();
+
+    let freq: i32 = freq_deltas
+        .iter()
         .sum();
 
-    println!("{}", freq);
+    println!("day 1 solution 1: {}", freq);
+
+    let mut delta_cycle = freq_deltas
+        .iter()
+        .cycle();
+
+    let mut freqs = HashSet::new();
+    let mut current_freq: i32 = 0;
+    freqs.insert(current_freq);
+
+    loop {
+        match delta_cycle.next() {
+            Some(d) => current_freq += d,
+            None => break,
+        }
+
+        if freqs.contains(&current_freq) {
+            break
+        }
+
+        freqs.insert(current_freq);
+    }
+    println!("day 1 solution 2: {}", current_freq);
 
     Ok(())
 }
